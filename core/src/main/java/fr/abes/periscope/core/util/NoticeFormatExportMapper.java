46b1c4f82e1c9117538099a91730ac8917b9105a
Pierre Maraval <maraval@abes.fr>
2022-04-06 09:03:15 +0200

    Refacto : Suppression code dupliqué dans les mapper noticesSolr et NoticeFormatExportMapper

--- core/src/main/java/fr/abes/periscope/core/util/NoticeFormatExportMapper.java
+++ core/src/main/java/fr/abes/periscope/core/util/NoticeFormatExportMapper.java
@@ -77,7 +77,7 @@ public class NoticeFormatExportMapper {
 
                                         // Extraction de la date de début
                                         try {
-                                            PublicationYear year = buildStartPublicationYear(value);
+                                            PublicationYear year = utilsMapper.buildStartPublicationYear(value);
                                             target.setStartYear(year);
                                         } catch (IllegalPublicationYearException e) {
                                             log.debug("Unable to parse start publication year :" + e.getLocalizedMessage());
@@ -86,7 +86,7 @@ public class NoticeFormatExportMapper {
 
                                         // Extraction de la date de fin
                                         try {
-                                            PublicationYear year = buildEndPublicationYear(value);
+                                            PublicationYear year = utilsMapper.buildEndPublicationYear(value);
                                             target.setEndYear(year);
                                         } catch (IllegalPublicationYearException e) {
                                             log.debug("Unable to parse end publication year :" + e.getLocalizedMessage());
@@ -416,138 +416,6 @@ public class NoticeFormatExportMapper {
             Sequence sequence = new SequenceLacune(startYear, volume, numero);
             holding.addSequence(sequence);
         }
-    }
-
-
-    /**
-     * Extrait l'année de début de publication
-     *
-     * @param value zone
-     * @return PublicationYear Année de début de publication
-     * @throws IllegalPublicationYearException si l'année de publication ne peut pas être décodée
-     */
-    public PublicationYear buildStartPublicationYear(String value) throws IllegalPublicationYearException {
-        String yearCode = value.substring(8, 9);
-        String candidateYear;
-        switch (yearCode) {
-            case "b":
-            case "a":
-            case "c":
-            case "d":
-            case "e":
-            case "g":
-            case "h":
-            case "i":
-            case "j":
-                candidateYear = value.substring(9, 13);
-                return extractDate(candidateYear);
-            case "f":
-                String candidateOldestYear = value.substring(9, 13);
-                String candidateNewestYear = value.substring(13, 17);
-                return extractCaseF(candidateOldestYear, candidateNewestYear);
-            default:
-                throw new IllegalPublicationYearException("Unable to decode year code " + yearCode);
-        }
-
-    }
-
-    /**
-     * Extrait l'année de fin de publication
-     *
-     * @param value zone
-     * @return PublicationYear Année de fin de publication
-     * @throws IllegalPublicationYearException si l'année de publication ne peut pas être décodée
-     */
-    public PublicationYear buildEndPublicationYear(String value) throws IllegalPublicationYearException {
-        String yearCode = value.substring(8, 9);
-        String candidateYear;
-
-        switch (yearCode) {
-            case "b":
-                candidateYear = value.substring(13, 17);
-                return extractDate(candidateYear);
-            case "a":
-                candidateYear = value.substring(13, 17);
-                if (candidateYear.equals("9999")) {
-                    return new PublicationYear(); // Année nulle par défaut
-                } else
-                    throw new IllegalPublicationYearException("Unable to decode end year code " + yearCode);
-            case "c":
-            case "d":
-                candidateYear = value.substring(13, 17);
-                if (candidateYear.equals("    ")) {
-                    return new PublicationYear(); // Année nulle par défaut
-                } else
-                    throw new IllegalPublicationYearException("Unable to decode end year code " + yearCode);
-            case "e":
-            case "f":
-            case "h":
-            case "i":
-            case "j":
-                return new PublicationYear();
-            case "g":
-                candidateYear = value.substring(13, 17);
-                if (candidateYear.equals("9999")) {
-                    return new PublicationYear(); // Année nulle par défaut
-                } else {
-                    return extractDate(candidateYear);
-                }
-            default:
-                throw new IllegalPublicationYearException("Unable to decode year code " + yearCode);
-        }
-    }
-
-    /**
-     * Extrait la date de publication
-     *
-     * @param candidateYear
-     * @return
-     * @throws IllegalPublicationYearException
-     */
-    private PublicationYear extractDate(String candidateYear) throws IllegalPublicationYearException {
-        PublicationYear year = new PublicationYear();
-        if (candidateYear.equals("    ")) return year;
-        if (candidateYear.charAt(2) == ' ' && candidateYear.charAt(3) == ' ') {
-            year.setYear(candidateYear.substring(0, 2) + "XX");
-            year.setConfidenceIndex(100);
-        } else if (candidateYear.charAt(2) == ' ') {
-            new IllegalPublicationYearException("Unable to decode year format like" + candidateYear);
-
-        } else if (candidateYear.charAt(3) == ' ') {
-            year.setYear(candidateYear.substring(0, 3) + "X");
-            year.setConfidenceIndex(10);
-        } else {
-            year.setYear(candidateYear.substring(0, 4));
-            year.setConfidenceIndex(0);
-        }
-        return year;
-    }
-
-    /**
-     * Extrait le cas F
-     *
-     * @param candidateOldestYear
-     * @param candidateNewestYear
-     * @return
-     * @throws IllegalPublicationYearException
-     */
-    private PublicationYear extractCaseF(String candidateOldestYear, String candidateNewestYear) throws IllegalPublicationYearException {
-        int cdtOldestYear = (candidateOldestYear.equals("    ")) ? 0 : Integer.parseInt(candidateOldestYear.trim());
-        int cdtNewestYear = (candidateNewestYear.equals("    ")) ? 9999 : Integer.parseInt(candidateNewestYear.trim());
-        PublicationYear year = new PublicationYear();
-        if (cdtOldestYear > cdtNewestYear) {
-            throw new IllegalPublicationYearException("Oldest Year can't be superior to newest Year");
-        }
-        if (cdtOldestYear == 0) {
-            year.setYear(candidateNewestYear);
-        } else {
-            year.setYear(candidateOldestYear);
-        }
-        if (cdtNewestYear != 9999 && cdtOldestYear != 0)
-            year.setConfidenceIndex(cdtNewestYear - cdtOldestYear);
-        else
-            year.setConfidenceIndex(0);
-        return year;
     }
 
     /**
