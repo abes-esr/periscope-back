c068532e5b1ff6fa69bbd7923fa59f40cd1d0141
SamuelQuetin <quetin@abes.fr>
2025-03-13 15:20:38 +0100

    modif batch pour prise en compte du profile

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,6 +2,7 @@
 
 # Paramètres par défaut du conteneur
 export PERISCOPE_BATCH_AT_STARTUP=${PERISCOPE_BATCH_AT_STARTUP:='0'}
+export PERISCOPE_PROFILE=${PERISCOPE_PROFILE}
 
 # Réglage de /etc/environment pour que les crontab s'exécutent avec les bonnes variables d'env
 echo "$(env)
@@ -10,7 +11,7 @@ LANG=en_US.UTF-8" > /etc/environment
 # Force le démarrage du batch au démarrage du conteneur
 if [ "$PERISCOPE_BATCH_AT_STARTUP" = "1" ]; then
   echo "-> Lancement de run_batch.sh au démarrage du conteneur"
-  /scripts/run_batch.sh
+  /scripts/run_batch.sh $PERISCOPE_PROFILE
 fi
 
 exec "$@"
--- docker/run_batch.sh
+++ docker/run_batch.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+  echo "Erreur : Veuillez fournir un profil Spring en argument."
+  exit 1
+fi
 unset SPRING_PROFILES_ACTIVE
-java -Djava.security.egd=file:///dev/urandom -jar periscope-batch-1.0.0-SNAPSHOT.jar --spring.batch.job.names=indexerTableNoticesBibio --spring.profiles.active=prod
+java -Djava.security.egd=file:///dev/urandom -jar periscope.jar --spring.batch.job.names=indexerTableNoticesBibio --spring.profiles.active=$1
